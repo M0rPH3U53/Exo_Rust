f23fa32ba92fe38a57811d078108b0788f456d09
M0rPH3U53 <144029241+M0rPH3U53@users.noreply.github.com>
2024-06-03 21:31:40 +0200

    ajout de ": i32"

--- exercises/01_variables/variables6.rs
+++ exercises/01_variables/variables6.rs
@@ -3,9 +3,13 @@
 // Execute `rustlings hint variables6` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 const NUMBER = 3;
+fn main() {
+    println!("Number {}", NUMBER);
+}
+
+// ajout de ": i32"
+const NUMBER: i32 = 3;
 fn main() {
     println!("Number {}", NUMBER);
 }
