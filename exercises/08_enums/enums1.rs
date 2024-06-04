27db7ea0f85b13f3ab8bc2e8a6b4071c4ecce708
M0rPH3U53 <144029241+M0rPH3U53@users.noreply.github.com>
2024-06-04 12:01:17 +0000

    ajout de 4 string dans "Message"

--- exercises/08_enums/enums1.rs
+++ exercises/08_enums/enums1.rs
@@ -4,9 +4,13 @@
 
 // I AM NOT DONE
 
+
 #[derive(Debug)]
 enum Message {
-    // TODO: define a few types of messages as used below
+    Quit,
+    Echo,
+    Move,
+    ChangeColor,
 }
 
 fn main() {
