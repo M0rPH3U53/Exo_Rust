d14bfac1aeca294ccfd7e8e6c74eb87d0dde5d94
M0rPH3U53 <144029241+M0rPH3U53@users.noreply.github.com>
2024-06-04 11:55:02 +0000

    ajout de la varible cents_per_gram dans la fonction get_fees

--- exercises/07_structs/structs3.rs
+++ exercises/07_structs/structs3.rs
@@ -31,12 +31,13 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
-        // Something goes here...
+    fn is_international(&self) -> bool {
+        self.sender_country != self.recipient_country
     }
 
-    fn get_fees(&self, cents_per_gram: u32) -> ??? {
+    fn get_fees(&self, cents_per_gram: u32) -> u32 {
         // Something goes here...
+        cents_per_gram * self.weight_in_grams
     }
 }
 
