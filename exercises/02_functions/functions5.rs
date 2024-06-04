15fa01d0c8b80f049654c5b27b506ad315bc173e
M0rPH3U53 <144029241+M0rPH3U53@users.noreply.github.com>
2024-06-04 07:49:39 +0000

    suppression de ";"

--- exercises/02_functions/functions5.rs
+++ exercises/02_functions/functions5.rs
@@ -3,8 +3,6 @@
 // Execute `rustlings hint functions5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
     let answer = square(3);
     println!("The square of 3 is {}", answer);
@@ -12,4 +10,15 @@ fn main() {
 
 fn square(num: i32) -> i32 {
     num * num;
+}
+
+// suppression de ";" a la fin de "num * num"
+
+fn main() {
+    let answer = square(3);
+    println!("The square of 3 is {}", answer);
+}
+
+fn square(num: i32) -> i32 {
+    num * num
 }
