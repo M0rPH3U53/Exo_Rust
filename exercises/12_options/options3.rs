06be7c36abae5cc17cd6ef943dd0d6a533b555cf
M0rPH3U53 <144029241+M0rPH3U53@users.noreply.github.com>
2024-06-04 12:36:42 +0000

    ajout de la string "&"

--- exercises/12_options/options3.rs
+++ exercises/12_options/options3.rs
@@ -12,8 +12,8 @@ struct Point {
 
 fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
-
-    match y {
+    // ajout de la string "&" avant "y"
+    match &y {
         Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => panic!("no match!"),
     }
