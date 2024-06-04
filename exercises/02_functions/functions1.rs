2edef774c4994be8480cd7ee8efad5aa79a4b50b
M0rPH3U53 <144029241+M0rPH3U53@users.noreply.github.com>
2024-06-04 10:23:54 +0000

    ajout de la fontion "call_me"

--- exercises/02_functions/functions1.rs
+++ exercises/02_functions/functions1.rs
@@ -3,11 +3,6 @@
 // Execute `rustlings hint functions1` or use the `hint` watch subcommand for a
 // hint.
 
-fn main() {
-    call_me();
-}
-
-// ajout de la fontion "call_me" , car dans le main il appelle la fonction call_me
 fn call_me() {}
 
 fn main() {
