0307f9cb17af24a9a61185e0cce2c25e4794c1f8
M0rPH3U53 <144029241+M0rPH3U53@users.noreply.github.com>
2024-06-04 10:27:38 +0000

    ajout d'une condition "if"

--- exercises/03_if/if1.rs
+++ exercises/03_if/if1.rs
@@ -8,37 +8,5 @@ pub fn bigger(a: i32, b: i32) -> i32 {
     // Do not use:
     // - another function call
     // - additional variables
-}
-
-// Don't mind this for now :)
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn ten_is_bigger_than_eight() {
-        assert_eq!(10, bigger(10, 8));
-    }
-
-    #[test]
-    fn fortytwo_is_bigger_than_thirtytwo() {
-        assert_eq!(42, bigger(32, 42));
-    }
-
-    #[test]
-    fn equal_numbers() {
-        assert_eq!(42, bigger(42, 42));
-    }
-}
-
-
-// ajout d'une condition "if" a la fin de la fonction bigger
-
-pub fn bigger(a: i32, b: i32) -> i32 {
-    // Complete this function to return the bigger number!
-    // If both numbers are equal, any of them can be returned.
-    // Do not use:
-    // - another function call
-    // - additional variables
     if a >= b { a } else { b }
 }
