5ce334e7b4b3b9f90ab6cd310b624cffdf5c1234
M0rPH3U53 <144029241+M0rPH3U53@users.noreply.github.com>
2024-06-04 10:18:07 +0000

    ajout de let

--- exercises/01_variables/variables1.rs
+++ exercises/01_variables/variables1.rs
@@ -5,12 +5,8 @@
 // Execute `rustlings hint variables1` or use the `hint` watch subcommand for a
 // hint.
 
-fn main() {
-    x = 5;
-    println!("x has the value {}", x);
-}
-
 // ajout de let devant x = 5;
+
 fn main() {
     let x = 5;
     println!("x has the value {}", x);
