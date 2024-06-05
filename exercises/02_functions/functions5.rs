35901cb4ef4509e80f217ccea5763cff4c0b83a3
LOCATELLI Romain <144029241+M0rPH3U53@users.noreply.github.com>
2024-06-05 12:01:46 +0000

    suppression du ";"

--- exercises/02_functions/functions5.rs
+++ exercises/02_functions/functions5.rs
@@ -3,7 +3,8 @@
 // Execute `rustlings hint functions5` or use the `hint` watch subcommand for a
 // hint.
 
-// suppression de la string ";" a la fin de "num * num"
+// suppression du ";" a la fin de "num * num" , pour que la fonction nous retourne la valeur souhaiter.
+// Un ";" indique la fin d'une instruction qui est "num * num" ne retournerais rien.
 
 fn main() {
     let answer = square(3);
