f679cca77e82beb8bf72de3005899344a237e051
LOCATELLI Romain <144029241+M0rPH3U53@users.noreply.github.com>
2024-06-05 13:23:19 +0000

    suppression du "&"

--- exercises/06_move_semantics/move_semantics6.rs
+++ exercises/06_move_semantics/move_semantics6.rs
@@ -5,7 +5,7 @@
 // Execute `rustlings hint move_semantics6` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
+
 
 fn main() {
     let data = "Rust is great!".to_string();
@@ -19,7 +19,9 @@ fn main() {
 fn get_char(data: &String) -> char {
     data.chars().last().unwrap()
 }
-// suppression dela string "&"
+
+// La suppression du "&" avant "mut" dans la focntion "tring_uppercase" permet de prendre la propriete de la chaine en entrer.
+
 // Should take ownership
 fn string_uppercase(mut data: String) {
     data = data.to_uppercase();
