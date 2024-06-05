dc24ec034f0ab6c3871627fb08a77382401b024c
LOCATELLI Romain <144029241+M0rPH3U53@users.noreply.github.com>
2024-06-05 13:46:02 +0000

    Update modules3.rs

--- exercises/10_modules/modules3.rs
+++ exercises/10_modules/modules3.rs
@@ -8,9 +8,10 @@
 // Execute `rustlings hint modules3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 // TODO: Complete this use statement
+
 // ajout de std & time pour utiliser l'heure systeme de Linux
 use std::time::{SystemTime, UNIX_EPOCH};
 
