ab1e8105a0f651a00a3afe5a2bda68663b536453
LOCATELLI Romain <144029241+M0rPH3U53@users.noreply.github.com>
2024-06-05 11:41:15 +0000

    ajout de i32 dans "const NUMBER: i32 = 3;"

--- exercises/01_variables/variables6.rs
+++ exercises/01_variables/variables6.rs
@@ -3,7 +3,9 @@
 // Execute `rustlings hint variables6` or use the `hint` watch subcommand for a
 // hint.
 
-// ajout de ": i32"
+// L'ajout de i32 dans "const NUMBER: i32 = 3;" est utiliser pour indiquer le type de données, ce qui n'est pas necesaire , 
+// mais cela est une bonne pratique pour eviter les problemes potentiel.
+
 const NUMBER: i32 = 3;
 fn main() {
     println!("Number {}", NUMBER);
